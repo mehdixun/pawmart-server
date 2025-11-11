7d192b0d87af0d544bdc67aa23e19ef752b22f2f
mehdixun <mehdixhasn@gmail.com>
2025-11-11 16:35:02 +0600

    mongdb added

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(express.json());
 const uri = "mongodb+srv://pawmart:FKZ87d9QnbJF6nix@cluster0.dy2dskh.mongodb.net/?appName=Cluster0";
 
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
 app.get('/', (req, res) => {
   res.send('pawmert server is running')
 })
@@ -25,8 +34,9 @@ async function run() {
     const db = client.db('pawmart')
     const productsCollection = db.collection('products');
 
-    app.post('/products', (req, res) => {
-        const newProducts = await productsCollection.inser
+    app.post('/products', async(req, res) => {
+        const newProducts = await productsCollection.insertOne(newProducts)
+        res.send(result)
     })
 
     await client.db("admin").command({ ping: 1 });
